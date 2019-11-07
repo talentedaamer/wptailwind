5595a300bad35a636935e160ac42bfc1a8f99bbd
Aamer Shahzad (talentedaamer) <talentedaamer@gmail.com>
2019-11-08 02:16:59 +0500

    update tailwind config

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,16 @@
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
     theme: {
         container: {
             center: true,
+        },
+        screens: {
+            xs: '320px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
         },
         fontFamily: {
             sans: [
@@ -15,6 +24,19 @@ module.exports = {
                 'Raleway',
                 'sans-serif',
             ],
+        },
+        colors: {
+            transparent: 'transparent',
+            primary: colors.indigo,
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            red: colors.red,
+            yellow: colors.yellow,
+            green: colors.green,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.purple,
         }
     },
     variants: {},
