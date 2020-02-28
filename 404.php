2cefc3e0820a76ac28d8a02624e936a5fefdf3de
Aamer Shahzad <talentedaamer@gmail.com>
2020-02-29 04:21:03 +0500

    complete 404 page

--- 404.php
+++ 404.php
@@ -10,23 +10,26 @@
  |
  */
 
-/**
- * exit if accessed directly
- */
+# exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+# get header.php
 get_header(); ?>
 
-<div <?php wptw_container_class(); ?>>
-	<?php
-	esc_html_e( 'Sorry Page could not be found.', 'wptailwind' );
-	
-	esc_html_e( 'The page is removed or URL changes. Please search by keyword.', 'wptailwind' );
-	
-	get_search_form();
-	?>
+<div <?php wptw_container_class( 'container px-4' ); ?>>
+    <div <?php wptw_content_sidebar_class(); ?>>
+        <div <?php wptw_content_class(); ?>>
+			<?php
+			/**
+			 * 404 page generic content
+			 * @hooked: wptw_not_found_do_content
+			 */
+			do_action( 'wptw_not_found_content' );
+			?>
+        </div>
+    </div>
 </div>
 
 <?php get_footer(); ?>
